030cfa305b6a5dc2ae1f4505bae502d34fe111c7
Griffin Kelley <gkelley@wisc.edu>
2019-01-27 16:48:11 -0600

    Finished Module 6

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
-import Person from './Person/Person.js'
+import Person from './Person/Person.js';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary.js';
 
 // This is a container class because we are manipulating the state.
 class App extends Component {
@@ -61,12 +62,14 @@ class App extends Component {
           {/* map() every in an array into something else  (Similar to foreach) */}
           {this.state.persons.map( (person, index) => {
             return(
-              <Person 
-                name={person.name} 
-                age={person.age}
-                click={ () => this.deletePersonHandler(index)}
-                changed={(event) => this.nameChangedHandler(event, person.id)}
-                key={person.id}/>
+              <ErrorBoundary  key={person.id}>
+                <Person 
+                  name={person.name} 
+                  age={person.age}
+                  click={ () => this.deletePersonHandler(index)}
+                  changed={(event) => this.nameChangedHandler(event, person.id)}
+                 />
+              </ErrorBoundary>
             );
           })}
         </div>
